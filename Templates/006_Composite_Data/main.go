cbe5b15c39acc3772c72e20f50b3dcdbdb4e3246
Constantinos Davanos <cdavanos@gmail.com>
2017-11-22 14:42:59 -0500

    slice, map, structs, struct slices

--- Templates/006_Composite_Data/main.go
+++ Templates/006_Composite_Data/main.go
@@ -8,22 +8,75 @@ import (
 
 var tpl *template.Template
 
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+type items struct {
+	Wisdom    []sage
+	Transport []car
+}
+
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
 func main() {
 
+	// slice
 	//sages := []string{"yo", "sup", "whatup"}
 
-	sages := map[string]string{
-		"India":    "Ghandi",
-		"America":  "MLK",
-		"Meditate": "Buddha",
-		"Love":     "Jesus",
-		"Prophet":  "Muhammad"}
+	// map
+	// sages := map[string]string{
+	// 	"India":    "Ghandi",
+	// 	"America":  "MLK",
+	// 	"Meditate": "Buddha",
+	// 	"Love":     "Jesus",
+	// 	"Prophet":  "Muhammad"}
 
-	err := tpl.Execute(os.Stdout, sages)
+	// struct
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	ghandi := sage{
+		Name:  "Ghandi",
+		Motto: "Be the change",
+	}
+
+	f := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+
+	c := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
+	// slice of structs
+	sages := []sage{buddha, ghandi}
+	cars := []car{f, c}
+
+	data := struct {
+		Wisdom    []sage
+		Transport []car
+	}{
+		sages,
+		cars,
+	}
+
+	err := tpl.Execute(os.Stdout, data)
 	if err != nil {
 		log.Fatalln(err)
 	}
